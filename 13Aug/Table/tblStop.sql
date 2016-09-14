ce7036d2496e9f977ce79dc12e88ff4936f94d47
trainee <trainee>
2016-09-14 11:27:36 +0530

    Forgotpassword Maps Destinations Improved

---
+++ 13Aug/Table/tblStop.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[tblStop] (
+     [ID]      INT          NOT NULL,
+     [cityId]  INT          NOT NULL,
+     [stopsAt] VARCHAR (20) NOT NULL,
+     PRIMARY KEY CLUSTERED ([ID] ASC),
+     FOREIGN KEY ([cityId]) REFERENCES [dbo].[tblCity] ([cityId])
+ );
---
+++ 13Aug/Table/tblUsers.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[tblUsers] (
+     [userId] INT NOT NULL,
+     [XP]     INT DEFAULT ((0)) NOT NULL,
+     [Rating] INT DEFAULT ((0)) NOT NULL,
+     [ID]     INT NOT NULL,
+     PRIMARY KEY CLUSTERED ([userId] ASC),
+     FOREIGN KEY ([ID]) REFERENCES [dbo].[tblRegistration] ([ID])
+ );
